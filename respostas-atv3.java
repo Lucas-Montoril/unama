9e1af7264c2f4bf9b23ba6ffd6252d0add60a1b6
Lucas-Montoril <fmontoril2@gmail.com>
2025-09-01 18:50:21 -0300

    Add files via upload

---
+++ respostas-atv3.java
@@ -0,0 +1,32 @@
+// 1) Suponha que você esteja criando um app para anotar os pedidos
+// dos clientes em um restaurante. Seu app precisa de uma lista de
+// pedidos. Os garçons adicionam os pedidos a essa lista e os chefes os
+// retiram da lista. Os garçons colocam os pedidos no final da lista e os
+// chefes retiram os pedidos do começo dela para cozinhá-los.
+// Você usaria um array ou uma lista encadeada para implementar o app?
+// Justifique
+//
+//
+// resposta: lista encadeada porque eu posso inserir algo no final ou remover no inicio sem se preocupar em deslocar os elementos
+
+// 2) Imagine que o Instagram guarda uma lista de usuários. Quando
+// alguém tenta acessá-lo, uma busca é feita pelo nome de usuário. Se o
+// nome da pessoas está na lista, ela pode continuar o acesso. As
+// pessoas usam o Instagram com muita frequência, então existem muitas
+// buscas nessa lista.
+// Presuma que o Instagram use um algoritmo de busca que requer
+// acesso aleatório, sendo que é necessário ser capaz de acessar o meio
+// da lista de nomes instantaneamente. Sabendo disso, você
+// implementaria essa lista usando um array ou uma lista encadeada?
+//
+//
+// resposta: array
+
+// 3) As pessoas se inscrevem no Instagram com muita frequência.
+// Suponha que você decida usar um array para armazenar a lista de
+// usuários. Quais as desvantagens de um array em relação às
+// inserções? O que acontece quando você adiciona novos usuários em
+// um array?
+//
+//
+// resposta: desvantagem é o custo de deslocamentos e realocações
